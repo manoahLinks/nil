bbdefc027edd60df96b631634191e855f8afed24
defistar <kanthl@nil.foundation>
2025-02-04 21:29:57 +0530

    feat: nix scripts for l1-contracts

---
+++ nix/l1-contracts.nix
@@ -0,0 +1,19 @@
+{ lib
+, stdenv
+}:
+let
+  inherit (lib) optional;
+in stdenv.mkDerivation {
+
+  name = "l1-contracts";
+  pname = "l1-contracts";
+
+  src = lib.sourceByRegex ./.. [
+    "^l1-contracts(/.*)?$"
+  ];
+
+  buildPhase = ''
+    cp -r ./l1-contracts/ $out/
+  '';
+
+}

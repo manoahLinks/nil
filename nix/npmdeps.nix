080f14aca6060a5510790fc902a853914022b5df
Andrei Malashkin <andrei.malashkin@nil.foundation>
2025-03-05 14:05:37 +0100

    add replace-in-file; update hash

--- nix/npmdeps.nix
+++ nix/npmdeps.nix
@@ -18,5 +18,5 @@ in
       ../uniswap/package.json
     ];
   };
-  hash = "sha256-FLQsE1cf4VJB/AY5i/XzZPdgyU8Fw3p9rK/z2DyZbkg=";
+  hash = "sha256-e7iO9irDTmiQiiy7zAksEbQ1LleObFaoqyt+3ws/eEE=";
 })

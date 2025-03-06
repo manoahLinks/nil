885daa21c6b549dcd78622bfc70fec39c9a10dd3
defistar <kanthl@nil.foundation>
2025-03-06 12:19:53 +0530

    feat: L2BridgeMessenger interfaces

--- rollup-bridge-contracts/contracts/bridge/l2/interfaces/IL2ERC20Bridge.sol
+++ rollup-bridge-contracts/contracts/bridge/l2/interfaces/IL2ERC20Bridge.sol
@@ -2,70 +2,62 @@
 pragma solidity 0.8.28;
 
 interface IL2ERC20Bridge {
-    /**
-     *
-     * Events *
-     *
-     */
+  /*//////////////////////////////////////////////////////////////////////////
+                             EVENTS   
+    //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Emitted when ERC20 token is deposited from L1 to L2 and transfer to recipient.
-    /// @param l1Token The address of the token in L1.
-    /// @param l2Token The address of the token in L2.
-    /// @param from The address of sender in L1.
-    /// @param to The address of recipient in L2.
-    /// @param feeRefundRecipient The address of excess-fee refund recipient on L2.
-    /// @param amount The amount of token withdrawn from L1 to L2.
-    /// @param data The optional calldata passed to recipient in L2.
-    event FinalizeDepositERC20(
-        address indexed l1Token,
-        address indexed l2Token,
-        address indexed from,
-        address to,
-        address feeRefundRecipient,
-        uint256 amount,
-        bytes data
-    );
+  /// @notice Emitted when ERC20 token is deposited from L1 to L2 and transfer to recipient.
+  /// @param l1Token The address of the token in L1.
+  /// @param l2Token The address of the token in L2.
+  /// @param from The address of sender in L1.
+  /// @param to The address of recipient in L2.
+  /// @param feeRefundRecipient The address of excess-fee refund recipient on L2.
+  /// @param amount The amount of token withdrawn from L1 to L2.
+  /// @param data The optional calldata passed to recipient in L2.
+  event FinalizeDepositERC20(
+    address indexed l1Token,
+    address indexed l2Token,
+    address indexed from,
+    address to,
+    address feeRefundRecipient,
+    uint256 amount,
+    bytes data
+  );
 
-    /**
-     *
-     * Public View Functions *
-     *
-     */
+  /*//////////////////////////////////////////////////////////////////////////
+                             PUBLIC CONSTANT FUNCTIONS   
+    //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Return the corresponding l1 token address given l2 token address.
-    /// @param l2Token The address of l2 token.
-    function getL1ERC20Address(address l2Token) external view returns (address);
+  /// @notice Return the corresponding l1 token address given l2 token address.
+  /// @param l2Token The address of l2 token.
+  function getL1ERC20Address(address l2Token) external view returns (address);
 
-    /// @notice Return the corresponding l2 token address given l1 token address.
-    /// @param l1Token The address of l1 token.
-    function getL2TokenAddress(address l1Token) external view returns (address);
+  /// @notice Return the corresponding l2 token address given l1 token address.
+  /// @param l1Token The address of l1 token.
+  function getL2TokenAddress(address l1Token) external view returns (address);
 
-    /**
-     *
-     * Public Mutating Functions *
-     *
-     */
+  /*//////////////////////////////////////////////////////////////////////////
+                             PUBLIC MUTATION FUNCTIONS   
+    //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Complete a deposit from L1 to L2 and send fund to recipient's account in L2.
-    /// @dev Make this function payable to handle WETH deposit/withdraw.
-    ///      The function should only be called by L2ScrollMessenger.
-    ///      The function should also only be called by L1ERC20Gateway in L1.
-    /// @param l1Token The address of corresponding L1 token.
-    /// @param l2Token The address of corresponding L2 token.
-    /// @param from The address of account who deposits the token in L1.
-    /// @param to The address of recipient in L2 to receive the token.
-    /// @param feeRefundRecipient The address of excess-fee refund recipient on L2.
-    /// @param amount The amount of the token to deposit.
-    /// @param data Optional data to forward to recipient's account.
-    function finalizeDepositERC20(
-        address l1Token,
-        address l2Token,
-        address from,
-        address to,
-        address feeRefundRecipient,
-        uint256 amount,
-        bytes calldata data
-    )
-        external
-        payable;
+  /// @notice Complete a deposit from L1 to L2 and send fund to recipient's account in L2.
+  /// @dev Make this function payable to handle WETH deposit/withdraw.
+  ///      The function should only be called by L2ScrollMessenger.
+  ///      The function should also only be called by L1ERC20Gateway in L1.
+  /// @param l1Token The address of corresponding L1 token.
+  /// @param l2Token The address of corresponding L2 token.
+  /// @param from The address of account who deposits the token in L1.
+  /// @param to The address of recipient in L2 to receive the token.
+  /// @param feeRefundRecipient The address of excess-fee refund recipient on L2.
+  /// @param amount The amount of the token to deposit.
+  /// @param data Optional data to forward to recipient's account.
+  function finalizeDepositERC20(
+    address l1Token,
+    address l2Token,
+    address from,
+    address to,
+    address feeRefundRecipient,
+    uint256 amount,
+    bytes calldata data
+  ) external payable;
 }
--- rollup-bridge-contracts/contracts/bridge/l2/interfaces/IL2ETHBridge.sol
+++ rollup-bridge-contracts/contracts/bridge/l2/interfaces/IL2ETHBridge.sol
@@ -2,33 +2,43 @@
 pragma solidity 0.8.28;
 
 interface IL2ETHBridge {
-    /// @notice Emitted when ETH is deposited from L1 to L2 and transfer to recipient.
-    /// @param l2Token The address of the ETH Token in L2.
-    /// @param from The address of sender in L1.
-    /// @param to The address of recipient in L2.
-    /// @param amount The amount of ETH withdrawn from L1 to L2.
-    /// @param data The optional calldata passed to recipient in L2.
-    event FinalizeDeposit(address indexed l2Token, address indexed from, address to, uint256 amount, bytes data);
+  /*//////////////////////////////////////////////////////////////////////////
+                             EVENTS   
+    //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Return the ETH token address on L2.
-    function getL2ETHAddress() external view returns (address);
+  /// @notice Emitted when ETH is deposited from L1 to L2 and transfer to recipient.
+  /// @param l2Token The address of the ETH Token in L2.
+  /// @param from The address of sender in L1.
+  /// @param to The address of recipient in L2.
+  /// @param amount The amount of ETH withdrawn from L1 to L2.
+  /// @param data The optional calldata passed to recipient in L2.
+  event FinalizeDeposit(address indexed l2Token, address indexed from, address to, uint256 amount, bytes data);
 
-    /// @notice Complete an ETH-deposit from L1 to L2 and send fund to recipient's account in L2.
-    /// @dev The function should only be called by L2ScrollMessenger.
-    /// @param l2Token The address of ETH Token on L2.
-    /// @param from The address of account who deposits the ETH in L1.
-    /// @param to The address of recipient in L2 to receive the ETH-Token.
-    /// @param feeRefundRecipient The address of excess-fee refund recipient on L2.
-    /// @param amount The amount of the ETH to deposit.
-    /// @param data Optional data to forward to recipient's account.
-    function finalizeETHDeposit(
-        address l2Token,
-        address from,
-        address to,
-        address feeRefundRecipient,
-        uint256 amount,
-        bytes calldata data
-    )
-        external
-        payable;
+  /*//////////////////////////////////////////////////////////////////////////
+                             PUBLIC CONSTANT FUNCTIONS   
+    //////////////////////////////////////////////////////////////////////////*/
+
+  /// @notice Return the ETH token address on L2.
+  function getL2ETHAddress() external view returns (address);
+
+  /*//////////////////////////////////////////////////////////////////////////
+                            PUBLIC MUTATION FUNCTIONS      
+    //////////////////////////////////////////////////////////////////////////*/
+
+  /// @notice Complete an ETH-deposit from L1 to L2 and send fund to recipient's account in L2.
+  /// @dev The function should only be called by L2ScrollMessenger.
+  /// @param l2Token The address of ETH Token on L2.
+  /// @param from The address of account who deposits the ETH in L1.
+  /// @param to The address of recipient in L2 to receive the ETH-Token.
+  /// @param feeRefundRecipient The address of excess-fee refund recipient on L2.
+  /// @param amount The amount of the ETH to deposit.
+  /// @param data Optional data to forward to recipient's account.
+  function finalizeETHDeposit(
+    address l2Token,
+    address from,
+    address to,
+    address feeRefundRecipient,
+    uint256 amount,
+    bytes calldata data
+  ) external payable;
 }
